2b861e4e7076b7d377dc0c11edd861a042064fac
mila <milamarcos98@gmail.com>
2024-05-27 14:43:02 -0300

    clase 15

---
+++ clase 15/src/components/Toast.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Toast.module.css'
+
+const Toast = () => {
+  return (
+    <div className={styles.container}>Toast</div>
+  )
+}
+
+export default Toast
